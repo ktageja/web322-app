0464a854a4a42bc47e227108963002ed574ca6c2
ktageja <tageja1kalki@gmail.com>
2023-02-03 12:15:39 -0500

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Kalki Tageja Student ID: 163983216 Date: ________________
+*  Name: Kalki Tageja Student ID: 163983216 Date: 2023-02-03
 *
 *  Online (Cyclic) Link: ________________________________________________________
 *
@@ -14,13 +14,14 @@ var express = require("express");
 var app = express();
 var path = require('path');
 var HTTP_PORT = process.env.PORT || 8080;
-var dataservice = require(__dirname + "/blog-service.js");
+var blog = require(__dirname + "/blog-service.js");
 
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.use(express.static('public'));
+app.use(express.static('public'));6532
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/about.html"));
@@ -28,11 +29,12 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/about.html"));
+    
 });
 
 
 app.get("/posts", (req, res) => {
-    dataservice.getPublishedPosts().then((data) => {
+    blog.getPublishedPosts().then((data) => {
         res.json({data});
     }).catch((err) => {
         res.json({message: err});
@@ -40,15 +42,15 @@ app.get("/posts", (req, res) => {
 });
 
 app.get("/blog", (req, res) => {
-    dataservice.getAllPosts().then((data) => {
+    blog.getAllPosts().then((data) => {
         res.json({data});
     }).catch((err) => {
         res.json({message: err});
     })
 });
 
-app.get("/Categories", (req, res) => {
-    dataservice.getCategories().then((data) => {
+app.get("/categories", (req, res) => {
+    blog.getCategories().then((data) => {
         res.json({data});
     }).catch((err) => {
         res.json({message: err});
@@ -59,7 +61,7 @@ app.use((req, res) => {
     res.status(404).end('404 PAGE NOT FOUND');
 });
 
-dataservice.initialize().then(() => {
+blog.initialize().then(() => {
     app.listen(HTTP_PORT, onHttpStart());
 }).catch (() => {
     console.log('promises unfulfilled');
